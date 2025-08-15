a9019780691bdce839fb0ffe46f7cc3cf848f0d3
Viktor <vikdemianovych@gmail.com>
2025-08-15 08:20:45 +0300

    working on api, and time to commit

--- taskmgr/settings.py
+++ taskmgr/settings.py
@@ -151,5 +151,5 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(hours=12),
 }
