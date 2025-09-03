4c94e2658c8da90158ddd1ce28abbb7af32fa2c3
Viktor <vikdemianovych@gmail.com>
2025-09-03 17:09:32 +0300

    getting the hang of it now, still doesn't work btw

--- taskmgr/celery.py
+++ taskmgr/celery.py
@@ -1,12 +1,27 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-
+from environs import Env
+# import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmgr.settings')
+
+from django.conf import settings
+# django.setup()
+
+settings.configure()
 
 result_backend = 'django-db'
 
-app = Celery('taskmgr', broker='pyamqp://guest@localhost//')
+env = Env()
+env.read_env()
+
+app = Celery('taskmgr', broker=f'pyamqp://{env.str("MQ_USER")}:{env.str("MQ_PASSWORD")}@localhost/vhost')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+app.autodiscover_tasks(related_name="tg_notifier")
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender: Celery, **kwargs):
+    from tasks.tg_notifier import send_deadline_notifications
+    sender.add_periodic_task(10.0, send_deadline_notifications.s(), name="send_deadline_notifications")
--- tasks/tg_notifier.py
+++ tasks/tg_notifier.py
@@ -3,40 +3,18 @@ import requests
 from celery import shared_task
 from datetime import datetime
 from django.utils.timezone import make_aware
-from .models import Task, UserProfile
-from celery import Celery
-from celery.schedules import crontab
+from .models import Task
+import logging
+import os
+from django.conf import settings
+settings.configure()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmgr.settings')
 
 env = Env()
 env.read_env()
 
 TELEGRAM_KEY = env("TELEGRAM_KEY")
-
-
-
-app = Celery()
-
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender: Celery, **kwargs):
-
-    # Executes every Monday morning at 7:30 a.m.
-    sender.add_periodic_task(
-        10.0, # crontab(hour=9, minute=0),
-        send_deadline_notifications.s(),
-    )
-
-
-
-def send_telegram_notification(chat_id, message):
-    url = f"https://api.telegram.org/bot{TELEGRAM_KEY}/sendMessage"
-    payload = {
-        "chat_id": chat_id,
-        "text": message,
-        "parse_mode": "Markdown",
-    }
-    response = requests.post(url, json=payload)
-    return response.json()
-
+REMINDERS_CHAT_IDS = env("REMINDER_CHAT_IDS", []).split(",")
 
 
 @shared_task
@@ -45,11 +23,22 @@ def send_deadline_notifications():
     upcoming_deadline = now 
 
     tasks_due_soon = Task.objects.filter(due_date__lte=upcoming_deadline)
-
+    logging.info("Attempting to send a reminder")
+    logging.info(f"Tasks due soon: {tasks_due_soon}")
     for task in tasks_due_soon:
         user = task.user
-        chat_id = 1096766653 #TEST, I'LL BE BURNED AT A STAKE IF I PUSH THIS TO GIT
 
-        if chat_id:
+        for chat_id in REMINDERS_CHAT_IDS:
             telegram_message = f"*{user.username}*, isn't there something you've forgotten to do? \n*{task.title}* is waiting."
-            send_telegram_notification(chat_id, telegram_message)
+            url = f"https://api.telegram.org/bot{TELEGRAM_KEY}/sendMessage"
+            payload = {
+                "chat_id": chat_id,
+                "text": telegram_message,
+                "parse_mode": "Markdown",
+            }
+            response = requests.post(url, json=payload)
+            if response.status_code == 200:
+                logging.info(f"Reminder about {task.title} sent to {user.username} successfully")
+            else:
+                logging.error(f"Failed to send reminder about {task.title} to {user.username}")
+                logging.error(response.json())
