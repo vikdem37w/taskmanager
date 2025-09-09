3461512104f832740d52fcc54aa76d186c33175c
Viktor <vikdemianovych@gmail.com>
2025-09-09 17:21:34 +0300

    toyed around with docker and logging, and everything in between

--- taskmgr/wsgi.py
+++ taskmgr/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taskmgr.settings")
 
 application = get_wsgi_application()
+call_command("migrate")
--- tasks/tg_notifier.py
+++ tasks/tg_notifier.py
@@ -1,9 +1,15 @@
 import aiogram
+import logging
 from taskmgr.settings import REMINDERS_CHAT_IDS, TELEGRAM_KEY
 
 
 async def send_notification(message):
     bot = aiogram.Bot(token=TELEGRAM_KEY)
+    logger = logging.getLogger(__name__)
     for chat_id in REMINDERS_CHAT_IDS:
-        await bot.send_message(chat_id=chat_id, text=message, parse_mode="Markdown")
+        try:
+            await bot.send_message(chat_id=chat_id, text=message, parse_mode="Markdown")
+            logger.info(f"Reminder to {chat_id} sent successfully")
+        except Exception as e:
+            logger.error(f"Error sending notification to chat {chat_id}: {e}")
     await bot.session.close()
